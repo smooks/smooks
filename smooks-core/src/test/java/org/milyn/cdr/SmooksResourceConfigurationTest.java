d714569ee209946d97fa5b35c881d2b500314927
Manish <manish.in.java@gmail.com>
2018-10-28 15:18:18 +0530

    Removed empty test.

--- smooks-core/src/test/java/org/milyn/cdr/SmooksResourceConfigurationTest.java
+++ smooks-core/src/test/java/org/milyn/cdr/SmooksResourceConfigurationTest.java
@@ -3,32 +3,31 @@
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Lesser General Public
-	License (version 2.1) as published by the Free Software 
+	License (version 2.1) as published by the Free Software
 	Foundation.
 
 	This library is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    
-	See the GNU Lesser General Public License for more details:    
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
 	http://www.gnu.org/licenses/lgpl.txt
 */
 
 package org.milyn.cdr;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.milyn.cdr.xpath.SelectorStepBuilder;
 import org.milyn.delivery.sax.SAXElement;
 import org.milyn.util.DomUtil;
 import org.milyn.xml.XmlUtil;
-import org.milyn.cdr.xpath.SelectorStep;
-import org.milyn.cdr.xpath.SelectorStepBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.helpers.AttributesImpl;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class SmooksResourceConfigurationTest {
 
@@ -41,11 +40,11 @@ public class SmooksResourceConfigurationTest {
 		resourceConfig.setParameter("y", "val y 1");
 		resourceConfig.setParameter("y", "val y 2");
 		assertEquals("Expected y to be 'val y 1'", "val y 1", resourceConfig.getParameter("y").getValue());
-		
+
 		List yParams = resourceConfig.getParameters("y");
 		assertEquals("val y 1", ((Parameter)yParams.get(0)).getValue());
 		assertEquals("val y 2", ((Parameter)yParams.get(1)).getValue());
-		
+
 		List allParams = resourceConfig.getParameterList();
 		assertEquals(2, allParams.size());
 		assertEquals("x", ((Parameter)allParams.get(0)).getName());
@@ -56,7 +55,7 @@ public class SmooksResourceConfigurationTest {
 	public void test_getBoolParameter() {
 		SmooksResourceConfiguration resourceConfig = new SmooksResourceConfiguration("body", "device", "xxx");
 		resourceConfig.setParameter("x", "true");
-		
+
 		assertTrue("Expected x to be true", resourceConfig.getBoolParameter("x", false));
 		assertFalse("Expected y to be false", resourceConfig.getBoolParameter("y", false));
 	}
@@ -65,11 +64,11 @@ public class SmooksResourceConfigurationTest {
 	public void test_getStringParameter() {
 		SmooksResourceConfiguration resourceConfig = new SmooksResourceConfiguration("body", "device", "xxx");
 		resourceConfig.setParameter("x", "xxxx");
-		
+
 		assertEquals("Expected x to be xxxx", "xxxx", resourceConfig.getStringParameter("x", "yyyy"));
 		assertEquals("Expected y to be yyyy", "yyyy", resourceConfig.getStringParameter("y", "yyyy"));
 	}
-    
+
 	@Test
     public void test_isTargetedAtElement_DOM() {
         Document doc = DomUtil.parse("<a><b><c><d><e/></d></c></b></a>");
@@ -160,7 +159,7 @@ public class SmooksResourceConfigurationTest {
         assertTrue(rc14.isTargetedAtElement(e, null));
         assertTrue(rc15.isTargetedAtElement(e, null));
         assertTrue(rc16.isTargetedAtElement(e, null));
-        
+
         assertTrue(!rc17.isTargetedAtElement(e, null));
         assertTrue(!rc18.isTargetedAtElement(e, null));
         assertTrue(!rc19.isTargetedAtElement(e, null));
@@ -277,7 +276,7 @@ public class SmooksResourceConfigurationTest {
     }
 
 	@Test
-    public void test_attributeSelector() {        
+    public void test_attributeSelector() {
         // Test that the attribute part of the selector doesn't get lowercased...
         SmooksResourceConfiguration resource = new SmooksResourceConfiguration("a/b/@myAttribute");
         assertEquals("a/b{@myAttribute}", SelectorStepBuilder.toString(resource.getSelectorSteps()));
@@ -323,13 +322,6 @@ public class SmooksResourceConfigurationTest {
         assertEquals("e", rc10.getTargetElement());
         assertEquals("attrib1", rc10.getTargetAttribute());
         assertTrue(!rc10.isTargetedAtElement(e, null));
-    }
-
-	@Test
-    public void test_hashedSelector() {
-        SmooksResourceConfiguration resource = new SmooksResourceConfiguration("#/@b");
-
-        System.out.println();
     }
 
     private SAXElement buildE() {
