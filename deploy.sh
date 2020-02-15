8464a339f310efe5206866299a1c79662f3771b0
claude.mamo <claude.mamo@gmail.com>
2020-02-15 12:46:43 +0100

    Automating publishing of artefact snapshots to Maven central repo

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-#
-# Run:
-# ./deploy.sh -u <repo-username> -p <repo-password> -g <passphrase-of-gpg-key>
-#
-
-while getopts u:p:g: option
-do
-    case "${option}"
-    in
-        u) USERNM=${OPTARG};;
-        p) PASSWD=${OPTARG};;
-        g) GPGPPH=${OPTARG};;
-    esac
-done
-
-export REPO_UN="$USERNM"
-export REPO_PW="$PASSWD"
-
-mvn clean deploy -Pdeploy -Dgpg.passphrase="$GPGPPH" -Dmaven.test.skip=true --settings ./settings_deploy.xml
+mvn clean deploy -Dgpg.skip=false -DskipTests=true -Dmaven.javadoc.skip=true -B
