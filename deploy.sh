33213a3a16cc981eb933829f6d58f5f513914047
Claude Mamo <claude.mamo@gmail.com>
2020-08-12 17:57:59 +0200

    Publishing Javadocs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean deploy --settings=.mvn/settings.xml -Dgpg.skip=false -DskipTests=true -Dmaven.javadoc.skip=true -B
+mvn clean deploy --settings=.mvn/settings.xml -Dgpg.skip=false -DskipTests=true -B
